a0032c1cbd6c962f1a67884ceb911e066b5f89c2
Starinin Andrey <starininandrey@gmail.com>
2022-06-10 19:39:24 +0300

    Add LogToFile

---
+++ Dict.App/LogToFile.cs
@@ -0,0 +1,16 @@
+﻿namespace Dict.App;
+
+public static class LogToFile
+{
+    private static void WriteToFile(string path, string message)
+    {
+        var file = new StreamWriter(path, true);
+        file.WriteLine($"{DateTime.Now:g}: {message}");
+        file.Close();
+    }
+
+    public static void Info(string path, string message)
+    {
+        WriteToFile(path, $"[INFO] {message}");
+    }
+}
